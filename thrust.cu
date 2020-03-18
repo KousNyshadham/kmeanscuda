b0af22b56ca9f870b491aaad13d4e05096a812f6
KousNyshadham <koustubh.nyshadham@gmail.com>
2020-03-18 15:13:06 -0500

    done with thrust

--- thrust.cu
+++ thrust.cu
@@ -7,6 +7,7 @@
 #include <thrust/functional.h>
 #include <thrust/sort.h>
 #include <thrust/scan.h>
+#include <thrust/copy.h>
 
 #include <string>
 #include <fstream>
@@ -37,32 +38,32 @@ void kmeans_srand(unsigned int funcseed) {
     nextstd = funcseed;
 }
 
-thrust::host_vector<double> hostdataset(0);
-thrust::host_vector<double> hostcentroids(0);
+thrust::host_vector<float> hostdataset(0);
+thrust::host_vector<float> hostcentroids(0);
 thrust::host_vector<int> hostlabels(0);
 
 struct findNearestCentroids{
 	int dims;
 	int num_cluster;
-	double* dataset;
-	double* centroids;
+	float* dataset;
+	float* centroids;
 	int * labels;
 
-	findNearestCentroids(int _dims, int _num_cluster, double* _dataset, double* _centroids, int* _labels):
+	findNearestCentroids(int _dims, int _num_cluster, float* _dataset, float* _centroids, int* _labels):
 	dims(_dims), num_cluster(_num_cluster), dataset(_dataset), centroids(_centroids), labels(_labels){};
 
 	__host__ __device__
 	void operator()(int i){
         int label = 0;
-        double dist=0;
-        double running = 0;
+        float dist=0;
+        float running = 0;
         for(int k = 0; k < dims; k++){
             running += pow(dataset[i*dims+k]-centroids[0*dims+k],2);
         }
         running = sqrt(running);
         dist = running;
         for(int j = 1; j < num_cluster; j++){
-            double inner = 0;
+            float inner = 0;
             for(int k = 0; k < dims; k++){
                 inner += pow(dataset[i*dims+k]-centroids[j*dims+k],2);
             }
@@ -78,11 +79,11 @@ struct findNearestCentroids{
 
 struct converged{
 	double threshold;
-	double* oldCentroids;
-	double* newCentroids;
+	float* oldCentroids;
+	float* newCentroids;
 	int* convergedVector;
 
-	converged(double _threshold, double* _oldCentroids, double* _newCentroids, int* _convergedVector):
+	converged(double _threshold, float* _oldCentroids, float* _newCentroids, int* _convergedVector):
 	threshold(_threshold), oldCentroids(_oldCentroids), newCentroids(_newCentroids), convergedVector(_convergedVector){};
 
 
@@ -111,51 +112,44 @@ void randomCentroids(){
 }
 
 struct newadd{
-	int label;
 	int dims;
-	double* newcentroids;
-	double* dataset;
+	int* labels;
+	float* centroids;
+	float* dataset;
 
-	newadd(int _dims, int _label, double* _centroids, double* _dataset):	
-	label(_label), newcentroids(_centroids), dataset(_dataset), dims(_dims){};
+	newadd(int _dims , float* _centroids, float* _dataset, int* _labels):	
+	centroids(_centroids), dataset(_dataset), dims(_dims), labels(_labels){};
 
-	__host__ __device__
+	__device__
 	void operator()(int i){
 		for(int j = 0; j < dims; j++){
-			newcentroids[label*dims+j] += dataset[i*dims+j];	
+			atomicAdd(&centroids[labels[i]*dims+j], dataset[i*dims+j]);
 		}
 	}
 	
 };
 
-struct bruh{
+struct divide{
 	int dims;
-	int n;
-	int num_cluster;
-	int* indices;
-	int* prefixSum;
-	double* centroids;
-	double* dataset;
+	float* centroids;
+	int* labelcounts;
 
-	bruh(int _n, int _dims, int _num_cluster, int* _indices, int* _prefixSum, double* _centroids, double* _dataset):
-	n(_n), dims(_dims),num_cluster(_num_cluster), indices(_indices), prefixSum(_prefixSum), centroids(_centroids), dataset(_dataset){};
+	divide(int _dims, float* _centroids, int* _labelcounts):
+	dims(_dims), centroids(_centroids), labelcounts(_labelcounts){};
 
-	__host__ __device__
+	__device__
 	void operator()(int i){
-		if(i == num_cluster-1){
-			thrust::for_each(indices + prefixSum[i], indices+n,newadd(dims, i, centroids, dataset));
+		for(int j = 0; j < dims; j++){
+			centroids[i*dims+j] = centroids[i*dims+j]/labelcounts[i];	
 		}
-		else{
-			thrust::for_each(indices + prefixSum[i], indices+prefixSum[i+1],newadd(dims, i, centroids, dataset));
-		}
-		
 	}
-
+	
 };
 
 void kmeans(){
-	thrust::device_vector<double> dataset(n*dims);
-	thrust::device_vector<double> centroids(num_cluster*dims);
+	thrust::device_vector<float> dataset(n*dims);
+	thrust::device_vector<float> centroids(num_cluster*dims);
+	thrust::device_vector<float> oldcentroids(num_cluster*dims);
 	thrust::device_vector<int> labels(n);
 	hostcentroids.resize(dims*num_cluster);
 	hostlabels.resize(n);
@@ -164,16 +158,16 @@ void kmeans(){
 	centroids = hostcentroids;
     int iterations = 0;
     bool done = false;
-	//double totalduration = 0;
+	float totalduration = 0;
     while(!done){
-		//auto start = high_resolution_clock::now();
-
+		auto start = high_resolution_clock::now();
 		//
 		//
 		// FIND NEAREST CENTROIDS 
 		//
 		//
-		thrust::device_vector<double> oldcentroids = centroids;
+		thrust::copy(centroids.begin(), centroids.end(), oldcentroids.begin());
+		thrust::device_vector<float> oldcentroids= centroids;
         iterations++;
 		thrust::fill(labels.begin(),labels.end(), 0);
 		thrust::device_vector<int> temp(n);
@@ -184,48 +178,40 @@ void kmeans(){
 		// AVERAGE LABELED CENTROIDS
 		//
 		//
-		thrust::device_vector<int> indices(n);
-		thrust::sequence(indices.begin(), indices.end());
-		thrust::sort_by_key(labels.begin(), labels.end(), indices.begin());
+		thrust::device_vector<int> unsortedlabels(n);
+		thrust::copy(labels.begin(), labels.end(), unsortedlabels.begin());
+		thrust::sort(labels.begin(), labels.end());
 		thrust::device_vector<int> ones(n);
 		thrust::fill(ones.begin(),ones.end(), 1);
 		thrust::device_vector<int> uniqueLabels(num_cluster);
 		thrust::device_vector<int> labelsCount(num_cluster);
 		thrust::reduce_by_key(labels.begin(), labels.end(), ones.begin(), uniqueLabels.begin(), labelsCount.begin());
-		thrust::device_vector<int> prefixSum(num_cluster);
-		thrust::exclusive_scan(labelsCount.begin(), labelsCount.end(), prefixSum.begin());
+		thrust::device_vector<int> ncount(n);
+		thrust::sequence(ncount.begin(),ncount.end());
 		thrust::fill(centroids.begin(), centroids.end(), 0);
-		thrust::device_vector<int> clusters(num_cluster);
-		thrust::for_each(clusters.begin(), clusters.end(), bruh(n, dims,num_cluster, thrust::raw_pointer_cast(indices.data()), thrust::raw_pointer_cast(prefixSum.data()), thrust::raw_pointer_cast(centroids.data()), thrust::raw_pointer_cast(dataset.data())));
-
-
-	hostcentroids=centroids;
-	hostlabels=labels;
-		break;
-			
-		
-        //centroids = averageLabeledCentroids();
+		thrust::for_each(ncount.begin(), ncount.end(), newadd(dims, thrust::raw_pointer_cast(centroids.data()), thrust::raw_pointer_cast(dataset.data()), thrust::raw_pointer_cast(unsortedlabels.data())));
+		thrust::device_vector<int> numclusters(num_cluster);
+		thrust::sequence(numclusters.begin(),numclusters.end());
+		thrust::for_each(numclusters.begin(),numclusters.end(), divide(dims, thrust::raw_pointer_cast(centroids.data()), thrust::raw_pointer_cast(labelsCount.data())));
 		//
 		//
 		// CONVERGE CENTROIDS
 		//
 		//
-		/*
 		thrust::device_vector<int> convergedVector(num_cluster*dims);
-		thrust::sequence(convergedVector.begin(), convergedVector.end())
+		thrust::sequence(convergedVector.begin(), convergedVector.end());
 		thrust::for_each(convergedVector.begin(), convergedVector.end(), converged(threshold, thrust::raw_pointer_cast(oldcentroids.data()),thrust::raw_pointer_cast(centroids.data()), thrust::raw_pointer_cast(convergedVector.data())));
 		int sum = thrust::reduce(convergedVector.begin(), convergedVector.end(), (int) 0, thrust::plus<int>());
         done = (iterations == max_num_iter || sum == 0);
-		*/
-		//auto stop = high_resolution_clock::now();
-		//double duration = duration_cast<microseconds>(stop - start).count(); 
-		//totalduration += duration;
+		auto stop = high_resolution_clock::now();
+		float duration = duration_cast<microseconds>(stop - start).count(); 
+		totalduration += duration;
+		hostcentroids=centroids;
+		hostlabels=unsortedlabels;
     }
-	/*
-	double time_per_iter_in_ms = totalduration/iterations;
+	float time_per_iter_in_ms = totalduration/iterations;
 	auto iter_to_converge = iterations;
 	printf("%d,%lf\n", iter_to_converge, time_per_iter_in_ms);
-	*/
 	if(flag){
 		for(int i = 0; i < num_cluster; i++){
 			printf("%d ", i);
